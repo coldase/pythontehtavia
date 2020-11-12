9c651fea80c86022ed9c7cd1fbb0d5e173f12487
coldase <jarzu@pm.me>
2020-11-12 13:17:28 +0200

    added api/fileio/joke thingy

---
+++ fileio/spessu.py
@@ -0,0 +1,38 @@
+#requests -kirjasto täytyy asentaa, "pip install requests"
+import requests
+import json
+
+#Vitsi classi
+class Vitsi:
+	def __init__(self, id, joke):
+		self.id = id
+		self.joke = joke
+
+#Hakee vitsit rajapinnasta
+def get_jokes():
+	url = "https://icanhazdadjoke.com/"
+	res = requests.get(url, headers={"Accept":"application/json"})
+	data = json.loads(res.text)
+	return data
+
+#Tekee listan Vitsi -olioita
+def make_joke_list():
+	jokes = []
+	for x in range(20):
+		vitsi = get_jokes()
+		jokes.append(Vitsi(vitsi["id"], vitsi["joke"]))
+
+	return jokes
+
+#Tulostaa kaikki vitsit
+def print_jokes():
+	for x in make_joke_list():
+		print(x.joke)
+
+#Tekee annetun nimisen .txt tiedoston ja lisää vitsit
+def tee_vitsi_tiedosto(filename):
+	with open(f'{filename}.txt', "w") as file:
+		for vitsi in make_joke_list():
+			file.write(vitsi.joke+"\n")
+
+tee_vitsi_tiedosto("testivitsi")
